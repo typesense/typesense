4c32dbfa0719a1d3b1b76fb80a8da9771eee2fa0
Kishore Nallan <kishore@kishorelive.com>
2017-07-09 16:52:10 +0530

    Unified docker build shell script.
    Builds the development image and the deployment image on the same script when an extra is passed.

--- docker-build.sh
+++ docker-build.sh
@@ -1,17 +1,37 @@
 #!/bin/sh
-set -o xtrace
 
-PROJECT_DIR=`dirname $0`
+set -ex
+PROJECT_DIR=`dirname $0 | while read a; do cd $a && pwd && break; done`
 
-if [ "$1" = "--clean" ]; then
+if [[ "$@" == *"--clean"* ]]; then
+  echo "Cleaning..."
   rm -rf $PROJECT_DIR/build
   mkdir $PROJECT_DIR/build
-
-  # rm -rf $PROJECT_DIR/external
-  # DOCKER_NO_CACHE=" --no-cache"
 fi
 
-docker build --file $PROJECT_DIR/docker/development.Dockerfile --tag wreally/typesense-development $DOCKER_NO_CACHE $PROJECT_DIR/docker
+if [[ "$@" == *"--depclean"* ]]; then
+  echo "Cleaning dependencies..."
+  rm -rf $PROJECT_DIR/external
+fi
 
-./dockcross --image wreally/typesense-development cmake -H$PROJECT_DIR -B$PROJECT_DIR/build
-./dockcross --image wreally/typesense-development make -C $PROJECT_DIR/build
+echo "Creating development image..."
+docker build --file $PROJECT_DIR/docker/development.Dockerfile --tag wreally/typesense-development:latest $PROJECT_DIR/docker
+
+echo "Building Typesense..."
+docker run -v $PROJECT_DIR:/typesense wreally/typesense-development cmake -H/typesense -B/typesense/build
+docker run -v $PROJECT_DIR:/typesense wreally/typesense-development make -C/typesense/build
+
+if [[ "$@" == *"--build-deploy-image"* ]]; then
+    echo "Creating deployment image for Typesense server..."
+
+    if [ -z "$TYPESENSE_IMG_VERSION" ]; then
+      echo "Need to set TYPESENSE_IMG_VERSION environment variable."
+      exit 1
+    fi
+
+    cp $PROJECT_DIR/docker/deployment.Dockerfile $PROJECT_DIR/build
+    docker build --file $PROJECT_DIR/build/deployment.Dockerfile --tag wreally/typesense-server:$TYPESENSE_IMG_VERSION \
+                        $PROJECT_DIR/build
+fi
+
+echo "Done... quitting."
--- docker/development.Dockerfile
+++ docker/development.Dockerfile
@@ -1,10 +1,28 @@
-FROM dockcross/linux-x64
+FROM ubuntu:12.04
 
-# Required for OpenSSL 1.0.2+
-RUN echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/jessie-backport.list
+RUN apt-get update
+RUN apt-get install -y build-essential
+RUN apt-get install -y python-software-properties
+RUN add-apt-repository -y ppa:ubuntu-toolchain-r/test
 RUN apt-get update
 
-RUN apt-get install -y -t jessie-backports openssl
-RUN apt-get install -y -t jessie-backports libssl-dev
+RUN apt-get install -y gcc-4.9
+RUN update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 50
+RUN apt-get install -y g++-4.9
+RUN update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 50
+
 RUN apt-get install -y zlib1g-dev
+RUN apt-get install -y liblist-compare-perl
+RUN apt-get install -y wget
+RUN wget https://openssl.org/source/openssl-1.0.2k.tar.gz -P /opt
+RUN tar -C /opt -xvzf /opt/openssl-1.0.2k.tar.gz
+RUN cd /opt/openssl-1.0.2k && sh ./config --prefix=/usr --openssldir=/usr zlib-dynamic
+RUN make -C /opt/openssl-1.0.2k depend
+RUN make -C /opt/openssl-1.0.2k -j4
+RUN make -C /opt/openssl-1.0.2k install
+
 RUN apt-get install -y libsnappy-dev
+
+RUN wget http://www.cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz -P /opt
+RUN tar -C /opt -xvzf /opt/cmake-3.3.2-Linux-x86_64.tar.gz
+RUN cp -r /opt/cmake-3.3.2-Linux-x86_64/* /usr
