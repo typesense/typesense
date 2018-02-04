d8f3b8da9e5e3d2aca65fa3e4fef048770e47851
Kishore Nallan <kishore@kishorelive.com>
2018-02-04 13:27:53 -0500

    Option to build binary tars for releases from the build script.

--- docker-build.sh
+++ docker-build.sh
@@ -3,6 +3,10 @@
 set -ex
 PROJECT_DIR=`dirname $0 | while read a; do cd $a && pwd && break; done`
 
+if [ -z "$TYPESENSE_VERSION" ]; then
+  TYPESENSE_VERSION="nightly"
+fi
+
 if [[ "$@" == *"--clean"* ]]; then
   echo "Cleaning..."
   rm -rf $PROJECT_DIR/build
@@ -18,21 +22,27 @@ fi
 echo "Creating development image..."
 docker build --file $PROJECT_DIR/docker/development.Dockerfile --tag typesense/typesense-development:latest $PROJECT_DIR/docker
 
-echo "Building Typesense..."
-docker run -it -v $PROJECT_DIR:/typesense typesense/typesense-development cmake -DCMAKE_BUILD_TYPE=Release -H/typesense -B/typesense/build
+echo "Building Typesense $TYPESENSE_VERSION..."
+docker run -it -v $PROJECT_DIR:/typesense typesense/typesense-development cmake -DTYPESENSE_VERSION=$TYPESENSE_VERSION \
+-DCMAKE_BUILD_TYPE=Release -H/typesense -B/typesense/build
+
 docker run -it -v $PROJECT_DIR:/typesense typesense/typesense-development make -C/typesense/build
 
 if [[ "$@" == *"--build-deploy-image"* ]]; then
-    echo "Creating deployment image for Typesense server..."
-
-    if [ -z "$TYPESENSE_IMG_VERSION" ]; then
-      echo "Need to set TYPESENSE_IMG_VERSION environment variable."
-      exit 1
-    fi
+    echo "Creating deployment image for Typesense $TYPESENSE_VERSION server ..."
 
     cp $PROJECT_DIR/docker/deployment.Dockerfile $PROJECT_DIR/build
-    docker build --file $PROJECT_DIR/build/deployment.Dockerfile --tag typesense/typesense:$TYPESENSE_IMG_VERSION \
+    docker build --file $PROJECT_DIR/build/deployment.Dockerfile --tag typesense/typesense:$TYPESENSE_VERSION \
                         $PROJECT_DIR/build
+fi
+
+if [[ "$@" == *"--create-binary"* ]]; then
+    OS_FAMILY=linux
+    RELEASE_NAME=typesense-server-$TYPESENSE_VERSION-$OS_FAMILY-amd64
+    mv $PROJECT_DIR/build/typesense-server $PROJECT_DIR/build/$RELEASE_NAME
+    printf `md5sum $PROJECT_DIR/build/${RELEASE_NAME} | cut -b-32` > $PROJECT_DIR/build/$RELEASE_NAME.md5.txt
+    tar -cvzf $PROJECT_DIR/build/$RELEASE_NAME.tar.gz -C $PROJECT_DIR/build $RELEASE_NAME $RELEASE_NAME.md5.txt
+    echo "Built binaries successfully."
 fi
 
 echo "Done... quitting."
