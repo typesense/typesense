ba68cd0ac55f885118186d3e6dc97be36a99d7b8
Kishore Nallan <kishore@kishorelive.com>
2018-07-30 21:28:23 +0530

    Use system namespaced build directories.

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,8 @@
 
 set -ex
 PROJECT_DIR=`dirname $0 | while read a; do cd $a && pwd && break; done`
+SYSTEM_NAME=Linux
+BUILD_DIR=build-$SYSTEM_NAME
 
 if [ -z "$TYPESENSE_VERSION" ]; then
   TYPESENSE_VERSION="nightly"
@@ -9,14 +11,14 @@ fi
 
 if [[ "$@" == *"--clean"* ]]; then
   echo "Cleaning..."
-  rm -rf $PROJECT_DIR/build
-  mkdir $PROJECT_DIR/build
+  rm -rf $PROJECT_DIR/$BUILD_DIR
+  mkdir $PROJECT_DIR/$BUILD_DIR
 fi
 
 if [[ "$@" == *"--depclean"* ]]; then
   echo "Cleaning dependencies..."
-  rm -rf $PROJECT_DIR/external-Linux
-  mkdir $PROJECT_DIR/external-Linux
+  rm -rf $PROJECT_DIR/external-$SYSTEM_NAME
+  mkdir $PROJECT_DIR/external-$SYSTEM_NAME
 fi
 
 echo "Creating development image..."
@@ -24,37 +26,37 @@ docker build --file $PROJECT_DIR/docker/development.Dockerfile --tag typesense/t
 
 echo "Building Typesense $TYPESENSE_VERSION..."
 docker run -it -v $PROJECT_DIR:/typesense typesense/typesense-development cmake -DTYPESENSE_VERSION=$TYPESENSE_VERSION \
--DCMAKE_BUILD_TYPE=Release -H/typesense -B/typesense/build
+-DCMAKE_BUILD_TYPE=Release -H/typesense -B/typesense/$BUILD_DIR
 
-docker run -it -v $PROJECT_DIR:/typesense typesense/typesense-development make -C/typesense/build
+docker run -it -v $PROJECT_DIR:/typesense typesense/typesense-development make -C/typesense/$BUILD_DIR
 
 if [[ "$@" == *"--build-deploy-image"* ]]; then
     echo "Creating deployment image for Typesense $TYPESENSE_VERSION server ..."
 
-    cp $PROJECT_DIR/docker/deployment.Dockerfile $PROJECT_DIR/build
-    docker build --file $PROJECT_DIR/build/deployment.Dockerfile --tag typesense/typesense:$TYPESENSE_VERSION \
-                        $PROJECT_DIR/build
+    cp $PROJECT_DIR/docker/deployment.Dockerfile $PROJECT_DIR/$BUILD_DIR
+    docker build --file $PROJECT_DIR/$BUILD_DIR/deployment.Dockerfile --tag typesense/typesense:$TYPESENSE_VERSION \
+                        $PROJECT_DIR/$BUILD_DIR
 fi
 
 if [[ "$@" == *"--create-binary"* ]]; then
     OS_FAMILY=linux
     RELEASE_NAME=typesense-server-$TYPESENSE_VERSION-$OS_FAMILY-amd64
-    printf `md5sum $PROJECT_DIR/build/typesense-server | cut -b-32` > $PROJECT_DIR/build/typesense-server.md5.txt
-    tar -cvzf $PROJECT_DIR/build/$RELEASE_NAME.tar.gz -C $PROJECT_DIR/build typesense-server typesense-server.md5.txt
-    echo "Built binary successfully: $PROJECT_DIR/build/$RELEASE_NAME.tar.gz"
+    printf `md5sum $PROJECT_DIR/$BUILD_DIR/typesense-server | cut -b-32` > $PROJECT_DIR/$BUILD_DIR/typesense-server.md5.txt
+    tar -cvzf $PROJECT_DIR/$BUILD_DIR/$RELEASE_NAME.tar.gz -C $PROJECT_DIR/$BUILD_DIR typesense-server typesense-server.md5.txt
+    echo "Built binary successfully: $PROJECT_DIR/$BUILD_DIR/$RELEASE_NAME.tar.gz"
 fi
 
 if [[ "$@" == *"--package-libs"* ]]; then
     OS_FAMILY=linux
     RELEASE_NAME=typesense-server-libs-$TYPESENSE_VERSION-$OS_FAMILY-amd64
-    LIBS=`cat $PROJECT_DIR/external-Linux/libs.txt`
+    LIBS=`cat $PROJECT_DIR/external-$SYSTEM_NAME/libs.txt`
     TAR_PATHS=""
     for lib in $LIBS; do
         TAR_PATHS="$TAR_PATHS -C $PROJECT_DIR/../$lib `ls $PROJECT_DIR/../$lib/*.a | xargs basename`"
     done
 
-    TAR_PATHS="$TAR_PATHS -C $PROJECT_DIR/build `ls $PROJECT_DIR/build/*.a | xargs basename`"
-    tar -cvzf $PROJECT_DIR/build/$RELEASE_NAME.tar.gz $TAR_PATHS
+    TAR_PATHS="$TAR_PATHS -C $PROJECT_DIR/$BUILD_DIR `ls $PROJECT_DIR/$BUILD_DIR/*.a | xargs basename`"
+    tar -cvzf $PROJECT_DIR/$BUILD_DIR/$RELEASE_NAME.tar.gz $TAR_PATHS
 fi
 
 echo "Done... quitting."
