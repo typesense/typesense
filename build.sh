3b5fc8b27845d09c8b935607ae44d47299df89d5
Kishore Nallan <kishore@kishorelive.com>
2018-07-30 21:28:23 +0530

    Use system namespaced build directories.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -ex
 PROJECT_DIR=`dirname $0 | while read a; do cd $a && pwd && break; done`
 SYSTEM_NAME=$(uname -s)
+BUILD_DIR=build-$SYSTEM_NAME
 
 if [ -z "$TYPESENSE_VERSION" ]; then
   TYPESENSE_VERSION="nightly"
@@ -10,8 +11,8 @@ fi
 
 if [[ "$@" == *"--clean"* ]]; then
   echo "Cleaning..."
-  rm -rf $PROJECT_DIR/build
-  mkdir $PROJECT_DIR/build
+  rm -rf $PROJECT_DIR/$BUILD_DIR
+  mkdir $PROJECT_DIR/$BUILD_DIR
 fi
 
 if [[ "$@" == *"--depclean"* ]]; then
@@ -20,15 +21,15 @@ if [[ "$@" == *"--depclean"* ]]; then
   mkdir $PROJECT_DIR/external-$SYSTEM_NAME
 fi
 
-cmake -DTYPESENSE_VERSION=$TYPESENSE_VERSION -DCMAKE_BUILD_TYPE=Release -H$PROJECT_DIR -B$PROJECT_DIR/build
-make -C $PROJECT_DIR/build
+cmake -DTYPESENSE_VERSION=$TYPESENSE_VERSION -DCMAKE_BUILD_TYPE=Release -H$PROJECT_DIR -B$PROJECT_DIR/$BUILD_DIR
+make -C $PROJECT_DIR/$BUILD_DIR
 
 if [[ "$@" == *"--create-binary"* ]]; then
     OS_FAMILY=$(echo `uname` | awk '{print tolower($0)}')
     RELEASE_NAME=typesense-server-$TYPESENSE_VERSION-$OS_FAMILY-amd64
-    printf `md5sum $PROJECT_DIR/build/typesense-server | cut -b-32` > $PROJECT_DIR/build/typesense-server.md5.txt
-    tar -cvzf $PROJECT_DIR/build/$RELEASE_NAME.tar.gz -C $PROJECT_DIR/build typesense-server typesense-server.md5.txt
-    echo "Built binary successfully: $PROJECT_DIR/build/$RELEASE_NAME.tar.gz"
+    printf `md5sum $PROJECT_DIR/$BUILD_DIR/typesense-server | cut -b-32` > $PROJECT_DIR/$BUILD_DIR/typesense-server.md5.txt
+    tar -cvzf $PROJECT_DIR/$BUILD_DIR/$RELEASE_NAME.tar.gz -C $PROJECT_DIR/$BUILD_DIR typesense-server typesense-server.md5.txt
+    echo "Built binary successfully: $PROJECT_DIR/$BUILD_DIR/$RELEASE_NAME.tar.gz"
 fi
 
 if [[ "$@" == *"--package-libs"* ]]; then
@@ -40,6 +41,6 @@ if [[ "$@" == *"--package-libs"* ]]; then
         TAR_PATHS="$TAR_PATHS -C $lib `ls $lib/*.a | xargs basename`"
     done
 
-    TAR_PATHS="$TAR_PATHS -C $PROJECT_DIR/build `ls $PROJECT_DIR/build/*.a | xargs basename`"
-    tar -cvzf $PROJECT_DIR/build/$RELEASE_NAME.tar.gz $TAR_PATHS
+    TAR_PATHS="$TAR_PATHS -C $PROJECT_DIR/$BUILD_DIR `ls $PROJECT_DIR/$BUILD_DIR/*.a | xargs basename`"
+    tar -cvzf $PROJECT_DIR/$BUILD_DIR/$RELEASE_NAME.tar.gz $TAR_PATHS
 fi
