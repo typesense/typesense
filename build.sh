d857e1064601f2a9e68b89300b63f8216d511f2a
kishorenc <kishorenc@gmail.com>
2019-09-17 12:16:35 +0530

    Upgrade to OpenSSL 1.1.1

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ fi
 cmake -DTYPESENSE_VERSION=$TYPESENSE_VERSION -DCMAKE_BUILD_TYPE=Release -H$PROJECT_DIR -B$PROJECT_DIR/$BUILD_DIR
 make -C $PROJECT_DIR/$BUILD_DIR
 
-if [[ "$@" == *"--create-binary"* ]]; then
+if [[ "$@" == *"--package-binary"* ]]; then
     OS_FAMILY=$(echo `uname` | awk '{print tolower($0)}')
     RELEASE_NAME=typesense-server-$TYPESENSE_VERSION-$OS_FAMILY-amd64
     printf `md5sum $PROJECT_DIR/$BUILD_DIR/typesense-server | cut -b-32` > $PROJECT_DIR/$BUILD_DIR/typesense-server.md5.txt
