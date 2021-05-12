1f6b4e3cd4b5b381eb7ed161bcc9a24e1fdb5279
Kishore Nallan <kishorenc@gmail.com>
2021-05-12 16:38:33 +0530

    Fix packing/unpacking of lat lng.

--- src/collection.cpp
+++ src/collection.cpp
@@ -726,7 +726,9 @@ Option<nlohmann::json> Collection::search(const std::string & query, const std::
                 return Option<nlohmann::json>(404, error);
             }
 
-            int64_t lat_lng = GeoPoint::pack_lat_lng(std::stod(geo_coords[0]), std::stod(geo_coords[1]));
+            double lat = std::stod(geo_coords[0]);
+            double lng = std::stod(geo_coords[1]);
+            int64_t lat_lng = GeoPoint::pack_lat_lng(lat, lng);
             sort_field_std.name = actual_field_name;
             sort_field_std.geopoint = lat_lng;
         }
