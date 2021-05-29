d6d83ec3ed2c22c6425602d3cc1c4a1706054c45
Kishore Nallan <kishorenc@gmail.com>
2021-05-29 20:52:02 +0530

    Resolve hostnames in nodes config to IP automatically.

---
+++ test/raft_server_test.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <string>
+#include "raft_server.h"
+
+TEST(RaftServerTest, ResolveNodesConfigWithHostNames) {
+    ASSERT_EQ("127.0.0.1:8107:8108,127.0.0.1:7107:7108,127.0.0.1:6107:6108",
+              ReplicationState::resolve_node_hosts("127.0.0.1:8107:8108,127.0.0.1:7107:7108,127.0.0.1:6107:6108"));
+
+    ASSERT_EQ("127.0.0.1:8107:8108,127.0.0.1:7107:7108,127.0.0.1:6107:6108",
+              ReplicationState::resolve_node_hosts("localhost:8107:8108,localhost:7107:7108,localhost:6107:6108"));
+
+    ASSERT_EQ("localhost:8107:8108localhost:7107:7108,127.0.0.1:6107:6108",
+              ReplicationState::resolve_node_hosts("localhost:8107:8108localhost:7107:7108,localhost:6107:6108"));
+}
