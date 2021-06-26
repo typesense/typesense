e1e3517bfb4a91e791cbe2ab91658caa2056f0f2
Kishore Nallan <kishorenc@gmail.com>
2021-06-26 15:50:18 +0530

    Clean up after delete by query is done.

--- include/core_api_utils.h
+++ include/core_api_utils.h
@@ -9,6 +9,12 @@ struct deletion_state_t {
     std::vector<std::pair<size_t, uint32_t*>> index_ids;
     std::vector<size_t> offsets;
     size_t num_removed;
+
+    ~deletion_state_t() {
+        for(auto& kv: index_ids) {
+            delete [] kv.second;
+        }
+    }
 };
 
 Option<bool> stateful_remove_docs(deletion_state_t* deletion_state, size_t batch_size, bool& done);
