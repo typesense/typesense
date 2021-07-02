40305791f8f630af1c4d7373063c671bd2f53e31
Kishore Nallan <kishorenc@gmail.com>
2021-07-02 10:35:31 +0530

    Remove deprecated default_resolution config for geo fields.

--- src/collection_manager.cpp
+++ src/collection_manager.cpp
@@ -31,18 +31,12 @@ Collection* CollectionManager::init_collection(const nlohmann::json & collection
             field_obj[fields::index] = true;
         }
 
-        // handle older records indexed before geo_resolution field introduction
-        if(field_obj.count(fields::geo_resolution) == 0) {
-            field_obj[fields::geo_resolution] = size_t(DEFAULT_GEO_RESOLUTION);
-        }
-
         if(field_obj.count(fields::locale) == 0) {
             field_obj[fields::locale] = "";
         }
 
         fields.push_back({field_obj[fields::name], field_obj[fields::type], field_obj[fields::facet],
-                          field_obj[fields::optional], field_obj[fields::index],
-                          field_obj[fields::geo_resolution], field_obj[fields::locale]});
+                          field_obj[fields::optional], field_obj[fields::index], field_obj[fields::locale]});
     }
 
     std::string default_sorting_field = collection_meta[Collection::COLLECTION_DEFAULT_SORTING_FIELD_KEY].get<std::string>();
