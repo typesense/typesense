aeb63c27437a3e82ab595c36d88fd845b6081c93
Kishore Nallan <kishorenc@gmail.com>
2021-11-13 13:55:12 +0530

    Fix search cutoff for wildcard filtering.

--- include/thread_local_vars.h
+++ include/thread_local_vars.h
@@ -1,6 +1,9 @@
 #include <chrono>
 
 extern thread_local int64_t write_log_index;
-extern thread_local std::chrono::high_resolution_clock::time_point begin;
+
+// These are used for circuit breaking search requests
+// NOTE: if you fork off main search thread, care must be taken to initialize these from parent thread values
+extern thread_local std::chrono::high_resolution_clock::time_point search_begin;
 extern thread_local int64_t search_stop_ms;
 extern thread_local bool search_cutoff;
