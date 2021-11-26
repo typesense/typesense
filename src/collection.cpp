b14458efd4874fe9e6599da6ae24876a58ec37e5
Kishore Nallan <kishorenc@gmail.com>
2021-11-26 19:35:50 +0530

    Handle updates of repeating records within the same batch.

--- src/collection.cpp
+++ src/collection.cpp
@@ -223,21 +223,27 @@ nlohmann::json Collection::add_many(std::vector<std::string>& json_lines, nlohma
         // NOTE: we overwrite the input json_lines with result to avoid memory pressure
 
         record.is_update = false;
+        bool repeated_doc = false;
 
         if(!doc_seq_id_op.ok()) {
             record.index_failure(doc_seq_id_op.code(), doc_seq_id_op.error());
         } else {
+            const std::string& doc_id = record.doc["id"].get<std::string>();
+            repeated_doc = (batch_doc_ids.find(doc_id) != batch_doc_ids.end());
+
+            if(repeated_doc) {
+                // when a document repeats, we send the batch until this document so that we can deal with conflicts
+                i--;
+                goto do_batched_index;
+            }
+
             record.is_update = !doc_seq_id_op.get().is_new;
+
             if(record.is_update) {
                 get_document_from_store(get_seq_id_key(seq_id), record.old_doc);
-            } else {
-                const std::string& doc_id = record.doc["id"].get<std::string>();
-                if(batch_doc_ids.find(doc_id) != batch_doc_ids.end()) {
-                    record.index_failure(400, "Document with `id` " + doc_id + " already exists in the batch.");
-                } else {
-                    batch_doc_ids.emplace(doc_id);
-                }
             }
+
+            batch_doc_ids.insert(doc_id);
 
             // if `fallback_field_type` or `dynamic_fields` is enabled, update schema first before indexing
             if(!fallback_field_type.empty() || !dynamic_fields.empty()) {
@@ -248,25 +254,11 @@ nlohmann::json Collection::add_many(std::vector<std::string>& json_lines, nlohma
             }
         }
 
-        /*
-        // check for memory threshold before allowing subsequent batches
-        if(is_exceeding_memory_threshold()) {
-            exceeds_memory_limit = true;
-        }
-
-        if(exceeds_memory_limit) {
-            nlohmann::json index_res;
-            index_res["error"] = "Max memory ratio exceeded.";
-            index_res["success"] = false;
-            index_res["document"] = json_line;
-            json_lines[i] = index_res.dump();
-            record.index_failure(500, "Max memory ratio exceeded.");
-        }
-        */
-
         index_records.emplace_back(std::move(record));
 
-        if((i+1) % index_batch_size == 0 || i == json_lines.size()-1) {
+        do_batched_index:
+
+        if((i+1) % index_batch_size == 0 || i == json_lines.size()-1 || repeated_doc) {
             batch_index(index_records, json_lines, num_indexed);
 
             // to return the document for the single doc add cases
