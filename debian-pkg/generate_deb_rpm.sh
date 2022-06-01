9a81a5bccf04f0697c9af7e40c77ae86a19f35cf
Kishore Nallan <kishorenc@gmail.com>
2022-06-01 15:07:43 +0530

    Parameterize arch in deb/rpm generator.

--- debian-pkg/generate_deb_rpm.sh
+++ debian-pkg/generate_deb_rpm.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# TS_VERSION is passed as an environment variable to the script
+# TSV is passed as an environment variable to the script
 
-if [ -z "$TS_VERSION" ]
+if [ -z "$TSV" ]
 then
-  echo "\$TS_VERSION is not provided. Quitting."
+  echo '$TSV is not provided. Quitting.'
   exit 1
+fi
+
+if [ -z "$ARCH" ]
+then
+  echo '$ARCH is not provided. Quitting.'
+  exit 1
+fi
+
+RPM_ARCH=$ARCH
+if [ "$ARCH" == "amd64" ]; then
+  RPM_ARCH="x86_64"
 fi
 
 set -ex
@@ -16,40 +27,40 @@ cp -r $CURR_DIR/typesense-server /tmp/typesense-deb-build
 
 # Download Typesense, extract and make it executable
 
-#curl -o /tmp/typesense-server-$TS_VERSION.tar.gz https://dl.typesense.org/releases/$TS_VERSION/typesense-server-$TS_VERSION-linux-amd64.tar.gz
-rm -rf /tmp/typesense-server-$TS_VERSION && mkdir /tmp/typesense-server-$TS_VERSION
-tar -xzf /typesense-core/build-Linux/typesense-server-$TS_VERSION-linux-amd64.tar.gz -C /tmp/typesense-server-$TS_VERSION
+#curl -o /tmp/typesense-server-$TSV.tar.gz https://dl.typesense.org/releases/$TSV/typesense-server-$TSV-linux-${ARCH}.tar.gz
+rm -rf /tmp/typesense-server-$TSV && mkdir /tmp/typesense-server-$TSV
+tar -xzf /typesense-core/build-Linux/typesense-server-$TSV-linux-${ARCH}.tar.gz -C /tmp/typesense-server-$TSV
 
-downloaded_hash=`md5sum /tmp/typesense-server-$TS_VERSION/typesense-server | cut -d' ' -f1`
-original_hash=`cat /tmp/typesense-server-$TS_VERSION/typesense-server.md5.txt`
+downloaded_hash=`md5sum /tmp/typesense-server-$TSV/typesense-server | cut -d' ' -f1`
+original_hash=`cat /tmp/typesense-server-$TSV/typesense-server.md5.txt`
 
 if [ "$downloaded_hash" == "$original_hash" ]; then
     mkdir -p /tmp/typesense-deb-build/typesense-server/usr/bin
-    cp /tmp/typesense-server-$TS_VERSION/typesense-server /tmp/typesense-deb-build/typesense-server/usr/bin
+    cp /tmp/typesense-server-$TSV/typesense-server /tmp/typesense-deb-build/typesense-server/usr/bin
 else
     >&2 echo "Typesense server binary is corrupted. Quitting."
     exit 1
 fi
 
-rm -rf /tmp/typesense-server-$TS_VERSION /tmp/typesense-server-$TS_VERSION.tar.gz
+rm -rf /tmp/typesense-server-$TSV /tmp/typesense-server-$TSV.tar.gz
 
-sed -i "s/\$VERSION/$TS_VERSION/g" `find /tmp/typesense-deb-build -maxdepth 10 -type f`
+sed -i "s/\$VERSION/$TSV/g" `find /tmp/typesense-deb-build -maxdepth 10 -type f`
 
 dpkg-deb -Zgzip -z6 \
-         -b /tmp/typesense-deb-build/typesense-server "/tmp/typesense-deb-build/typesense-server-${TS_VERSION}-amd64.deb"
+         -b /tmp/typesense-deb-build/typesense-server "/tmp/typesense-deb-build/typesense-server-${TSV}-${ARCH}.deb"
 
 # Generate RPM
 
 rm -rf /tmp/typesense-rpm-build && mkdir /tmp/typesense-rpm-build
-cp "/tmp/typesense-deb-build/typesense-server-${TS_VERSION}-amd64.deb" /tmp/typesense-rpm-build
-cd /tmp/typesense-rpm-build && alien --scripts -k -r -g -v /tmp/typesense-rpm-build/typesense-server-${TS_VERSION}-amd64.deb
+cp "/tmp/typesense-deb-build/typesense-server-${TSV}-${ARCH}.deb" /tmp/typesense-rpm-build
+cd /tmp/typesense-rpm-build && alien --scripts -k -r -g -v /tmp/typesense-rpm-build/typesense-server-${TSV}-${ARCH}.deb
 
 sed -i 's#%dir "/"##' `find /tmp/typesense-rpm-build/*/*.spec -maxdepth 10 -type f`
 sed -i 's#%dir "/usr/bin/"##' `find /tmp/typesense-rpm-build/*/*.spec -maxdepth 10 -type f`
 sed -i 's/%config/%config(noreplace)/g' `find /tmp/typesense-rpm-build/*/*.spec -maxdepth 10 -type f`
 
-SPEC_FILE="/tmp/typesense-rpm-build/typesense-server-${TS_VERSION}/typesense-server-${TS_VERSION}-1.spec"
-SPEC_FILE_COPY="/tmp/typesense-rpm-build/typesense-server-${TS_VERSION}/typesense-server-${TS_VERSION}-copy.spec"
+SPEC_FILE="/tmp/typesense-rpm-build/typesense-server-${TSV}/typesense-server-${TSV}-1.spec"
+SPEC_FILE_COPY="/tmp/typesense-rpm-build/typesense-server-${TSV}/typesense-server-${TSV}-copy.spec"
 
 cp $SPEC_FILE $SPEC_FILE_COPY
 
@@ -73,9 +84,9 @@ tail -n+$START_LINE $SPEC_FILE_COPY >> $SPEC_FILE
 
 rm $SPEC_FILE_COPY
 
-cd /tmp/typesense-rpm-build/typesense-server-${TS_VERSION} && \
-  rpmbuild --target=x86_64 --buildroot /tmp/typesense-rpm-build/typesense-server-${TS_VERSION} -bb \
+cd /tmp/typesense-rpm-build/typesense-server-${TSV} && \
+  rpmbuild --target=${RPM_ARCH} --buildroot /tmp/typesense-rpm-build/typesense-server-${TSV} -bb \
   $SPEC_FILE
 
-cp "/tmp/typesense-rpm-build/typesense-server-${TS_VERSION}-amd64.deb" /typesense-core
-cp "/tmp/typesense-rpm-build/typesense-server-${TS_VERSION}-1.x86_64.rpm" /typesense-core
+cp "/tmp/typesense-rpm-build/typesense-server-${TSV}-${ARCH}.deb" /typesense-core
+cp "/tmp/typesense-rpm-build/typesense-server-${TSV}-1.${RPM_ARCH}.rpm" /typesense-core
