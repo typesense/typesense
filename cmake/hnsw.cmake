b8c11341c79c3e7b454b69afa53c90fcc4d32489
Kishore Nallan <kishorenc@gmail.com>
2022-11-02 14:39:09 +0530

    Bump HNSW dep: fixes early truncation of results.

--- cmake/hnsw.cmake
+++ cmake/hnsw.cmake
@@ -1,6 +1,6 @@
 # Download hnsw (header-only)
 
-set(HNSW_VERSION 7f971d46d6c14c187e6ad8cf237e8e121aae2257)
+set(HNSW_VERSION 21de18ffabea1a9d1e8b16b49afc6045d7707e4c)
 set(HNSW_NAME hnswlib-${HNSW_VERSION})
 set(HNSW_TAR_PATH ${DEP_ROOT_DIR}/${HNSW_NAME}.tar.gz)
 
