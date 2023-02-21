fe4c888adb9de2b371375d66ff0b1f64f8492d89
ozanarmagan <ozan-armagan@yandex.com>
2023-02-21 17:23:27 +0300

    Fix for CMake build II

--- cmake/onnxruntime_ext.cmake
+++ cmake/onnxruntime_ext.cmake
@@ -12,7 +12,7 @@ endif()
 ExternalProject_Add(
     onnxruntime_ext
     GIT_REPOSITORY https://github.com/microsoft/onnxruntime-extensions
-    GIT_TAG 81e7799c69044c745239202085eb0a98f102937b
+    GIT_TAG origin/rel-0.6.0
     SOURCE_DIR ${DEP_ROOT_DIR}/${ONNX_EXT_NAME}
     PATCH_COMMAND cd ${DEP_ROOT_DIR}/${ONNX_EXT_NAME} && git apply ${CMAKE_CURRENT_SOURCE_DIR}/cmake/onnx_ext.patch || git apply ${CMAKE_CURRENT_SOURCE_DIR}/cmake/onnx_ext.patch -R --check && cd operators && mkdir -p src_dir && mkdir -p src_dir/tokenizer && cp base64.h base64.cc string_utils_onnx.h string_utils_onnx.cc ustring.h ustring.cc src_dir && cp tokenizer/bert_tokenizer.hpp tokenizer/bert_tokenizer.cc tokenizer/basic_tokenizer.hpp tokenizer/basic_tokenizer.cc src_dir/tokenizer 
     CONFIGURE_COMMAND ""
