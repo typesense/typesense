bca7f69133ecc43f36805bd00674e2154b74745a
Harpreet Sangar <happy_san@protonmail.com>
2023-03-18 17:25:25 +0530

    Add `filter_result_iterator_t`.

---
+++ include/filter.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include "index.h"
+
+class filter_result_iterator_t {
+private:
+    std::string collection_name;
+    const Index* index;
+    filter_node_t* filter_node;
+    filter_result_iterator_t* left_it = nullptr;
+    filter_result_iterator_t* right_it = nullptr;
+
+    // Used in case of id and reference filter.
+    uint32_t result_index = 0;
+
+    // Stores the result of the filters that cannot be iterated.
+    filter_result_t filter_result;
+
+    // Initialized in case of filter on string field.
+    // Sample filter values: ["foo bar", "baz"]. Each filter value is split into tokens. We get posting list iterator
+    // for each token.
+    //
+    // Multiple filter values: Multiple tokens: posting list iterator
+    std::vector<std::vector<posting_list_t::iterator_t>> posting_list_iterators;
+    std::vector<posting_list_t*> expanded_plists;
+
+    // Set to false when this iterator or it's subtree becomes invalid.
+    bool is_valid = true;
+
+    /// Initializes the state of iterator node after it's creation.
+    void init();
+
+    /// Performs AND on the subtrees of operator.
+    void and_filter_iterators();
+
+    /// Performs OR on the subtrees of operator.
+    void or_filter_iterators();
+
+    /// Finds the next match for a filter on string field.
+    void doc_matching_string_filter();
+
+public:
+    uint32_t doc;
+    // Collection name -> references
+    std::map<std::string, reference_filter_result_t> reference;
+    Option<bool> status;
+
+    explicit filter_result_iterator_t(const std::string& collection_name,
+                                      const Index* index, filter_node_t* filter_node,
+                                      Option<bool>& status) :
+                                      collection_name(collection_name),
+                                      index(index),
+                                      filter_node(filter_node),
+                                      status(status) {
+        // Generate the iterator tree and then initialize each node.
+        if (filter_node->isOperator) {
+            left_it = new filter_result_iterator_t(collection_name, index, filter_node->left, status);
+            right_it = new filter_result_iterator_t(collection_name, index, filter_node->right, status);
+        }
+
+        init();
+    }
+
+    ~filter_result_iterator_t() {
+        // In case the filter was on string field.
+        for(auto expanded_plist: expanded_plists) {
+            delete expanded_plist;
+        }
+
+        delete left_it;
+        delete right_it;
+    }
+
+    [[nodiscard]] bool valid();
+
+    void next();
+
+    void skip_to(uint32_t id);
+};
