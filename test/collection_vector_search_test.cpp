8d92ad9cbf5bf26f7cc521599dc153d6eee8f6fd
ozanarmagan <o.armagan2020@gtu.edu.tr>
2023-08-03 14:02:39 +0300

    Fix 'HybridSearchWithExplicitVector' test

--- test/collection_vector_search_test.cpp
+++ test/collection_vector_search_test.cpp
@@ -775,7 +775,7 @@ TEST_F(CollectionVectorTest, HybridSearchWithExplicitVector) {
     ASSERT_EQ(2, search_res["found"].get<size_t>());
     ASSERT_EQ(2, search_res["hits"].size());
 
-    ASSERT_FLOAT_EQ(0.046207964, search_res["hits"][0]["vector_distance"].get<float>());
+    ASSERT_FLOAT_EQ(0.04620, search_res["hits"][0]["vector_distance"].get<float>());
     ASSERT_FLOAT_EQ(0.1213316321, search_res["hits"][1]["vector_distance"].get<float>());
 
     // to pass k param
