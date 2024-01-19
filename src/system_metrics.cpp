dba70e63c87b1aa6519da44a4ffd84f8908d325b
krunal <krunal1313@gmail.com>
2024-01-19 20:13:51 +0530

    add macro to run on linux only

--- src/system_metrics.cpp
+++ src/system_metrics.cpp
@@ -72,11 +72,12 @@ void SystemMetrics::get(const std::string &data_dir_path, nlohmann::json &result
     result["system_memory_total_bytes"] = std::to_string(get_memory_total_bytes());
     result["system_memory_used_bytes"] = std::to_string(get_memory_used_bytes());
 
+#ifdef __linux__
     auto swap_bytes = linux_get_swap_used_bytes();
     if(swap_bytes >= 0) {
         result["system_memory_used_swap_bytes"] = std::to_string(swap_bytes);
     }
-
+#endif
     // CPU and Network metrics
 #if __linux__
     const std::vector<cpu_stat_t>& cpu_stats = get_cpu_stats();
