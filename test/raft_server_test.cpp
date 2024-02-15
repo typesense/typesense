6e2931bd020eb59a55b421d0b5319b3c8b1c5183
Kishore Nallan <kishorenc@gmail.com>
2024-02-15 17:19:36 +0530

    Add guard for long hostnames.

--- test/raft_server_test.cpp
+++ test/raft_server_test.cpp
@@ -11,4 +11,9 @@ TEST(RaftServerTest, ResolveNodesConfigWithHostNames) {
 
     ASSERT_EQ("localhost:8107:8108localhost:7107:7108,127.0.0.1:6107:6108",
               ReplicationState::resolve_node_hosts("localhost:8107:8108localhost:7107:7108,localhost:6107:6108"));
+
+    // hostname must be less than 64 chars
+    ASSERT_EQ("",
+              ReplicationState::resolve_node_hosts("typesense-node-2.typesense-service.typesense-"
+                                                   "namespace.svc.cluster.local:6107:6108"));
 }
