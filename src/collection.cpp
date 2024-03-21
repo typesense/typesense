8a332ceaf1c2d11037292ebcea0604ecbbae3909
ozanarmagan <o.armagan2020@gtu.edu.tr>
2024-03-21 18:29:34 +0300

    Fix document store flag bug

--- src/collection.cpp
+++ src/collection.cpp
@@ -756,7 +756,7 @@ void Collection::batch_index(std::vector<index_record>& index_records, std::vect
             if(index_record.is_update) {
                 remove_flat_fields(index_record.new_doc);
                 for(auto& field: fields) {
-                    if(!field.store) {
+                    if(!field.store && index_record.new_doc.count(field.name) != 0) {
                         index_record.new_doc.erase(field.name);
                     }
                 }
@@ -778,7 +778,11 @@ void Collection::batch_index(std::vector<index_record>& index_records, std::vect
             } else {
                 // remove flattened field values before storing on disk
                 remove_flat_fields(index_record.doc);
-
+                for(auto& field: fields) {
+                    if(!field.store && index_record.doc.count(field.name) != 0) {
+                        index_record.doc.erase(field.name);
+                    }
+                }
                 const std::string& seq_id_str = std::to_string(index_record.seq_id);
                 const std::string& serialized_json = index_record.doc.dump(-1, ' ', false,
                                                                            nlohmann::detail::error_handler_t::ignore);
