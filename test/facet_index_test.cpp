cdb655289831a8c7f874c70992a520de8305ea03
Kishore Nallan <kishorenc@gmail.com>
2024-06-11 21:42:02 +0530

    Fix facet value hash mapping being cleared on sparsification.
    We still need it for maintaning str -> id mapping.

--- test/facet_index_test.cpp
+++ test/facet_index_test.cpp
@@ -30,6 +30,32 @@ TEST(FacetIndexTest, FacetValueDeletionString) {
     ASSERT_FALSE(findex.facet_value_exists("brand", "nike"));
 }
 
+TEST(FacetIndexTest, HighCardinalityCheck) {
+    facet_index_t findex;
+
+    for(size_t i = 0; i < 101; i++) {
+        findex.initialize("field_" + std::to_string(i));
+    }
+
+    std::unordered_map<facet_value_id_t, std::vector<uint32_t>, facet_value_id_t::Hash> fvalue_to_seq_ids;
+    std::unordered_map<uint32_t, std::vector<facet_value_id_t>> seq_id_to_fvalues;
+
+    facet_value_id_t nike("nike", 1);
+
+    fvalue_to_seq_ids[nike] = {0, 1, 2};
+    seq_id_to_fvalues[0] = {nike};
+    seq_id_to_fvalues[1] = {nike};
+    seq_id_to_fvalues[2] = {nike};
+
+    findex.insert("field_1", fvalue_to_seq_ids, seq_id_to_fvalues, true);
+    ASSERT_EQ(3, findex.facet_val_num_ids("field_1", "nike"));
+
+    findex.check_for_high_cardinality("field_1", 20000);
+    ASSERT_TRUE(findex.facet_value_exists("field_1", "nike"));
+
+    ASSERT_EQ(0, findex.facet_val_num_ids("field_1", "nike"));
+}
+
 TEST(FacetIndexTest, FacetValueDeletionOfLongString) {
     facet_index_t findex;
     findex.initialize("brand");
