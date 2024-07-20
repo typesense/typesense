586615316c9d20778c9c95e882c3c88e7e76c57c
ozanarmagan <o.armagan2020@gtu.edu.tr>
2024-07-20 10:49:42 +0300

    Add test

--- test/collection_vector_search_test.cpp
+++ test/collection_vector_search_test.cpp
@@ -3383,6 +3383,51 @@ TEST_F(CollectionVectorTest, TryAddingMultipleImageFieldToEmbedFrom) {
     ASSERT_TRUE(collection_create_op.ok());
 }
 
+TEST_F(CollectionVectorTest, TestLongTextForImageEmbedding) {
+    auto schema_json = R"({
+            "name": "images",
+            "fields": [
+                {
+                "name": "name",
+                "type": "string"
+                },
+                {
+                "name": "image",
+                "type": "image",
+                "store": false
+                },
+                {
+                "name": "embedding",
+                "type": "float[]",
+                "embed": {
+                    "from": [
+                    "image",
+                    "name"
+                    ],
+                    "model_config": {
+                    "model_name": "ts/clip-vit-b-p32"
+                    }
+                }
+                }
+            ]
+            })"_json;
+    
+    EmbedderManager::set_model_dir("/tmp/typesense_test/models");
+
+    auto collection_create_op = collectionManager.create_collection(schema_json);
+    ASSERT_TRUE(collection_create_op.ok());
+    auto coll = collection_create_op.get();
+
+    const std::string long_text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+
+    nlohmann::json doc;
+    doc["name"] = long_text;
+
+    auto add_op = coll->add(doc.dump());
+    ASSERT_TRUE(add_op.ok());
+}
+
+
 TEST_F(CollectionVectorTest, TestMultipleFieldsForImageEmbedding) {
     auto schema_json = R"({
             "name": "images",
