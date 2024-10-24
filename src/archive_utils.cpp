5ac028848c7f3eda02d21461fecf75f1fa975d97
Harisaran G <harithiru2018@gmail.com>
2024-10-24 17:24:32 +0530

    fix: memory leaks

--- src/archive_utils.cpp
+++ src/archive_utils.cpp
@@ -6,8 +6,8 @@
 #include <stdexcept>
 
 bool ArchiveUtils::extract_tar_gz_from_file(const std::string& archive_path, const std::string& destination_path) {
-    struct archive* a;
-    struct archive* ext;
+    struct archive* a = nullptr;
+    struct archive* ext = nullptr;
     struct archive_entry* entry;
     int flags;
     int r;
@@ -15,10 +15,19 @@ bool ArchiveUtils::extract_tar_gz_from_file(const std::string& archive_path, con
     flags = ARCHIVE_EXTRACT_TIME | ARCHIVE_EXTRACT_PERM | ARCHIVE_EXTRACT_ACL | ARCHIVE_EXTRACT_FFLAGS;
 
     a = archive_read_new();
+    if (!a) {
+        return false;
+    }
+
     archive_read_support_format_all(a);
     archive_read_support_filter_all(a);
 
     ext = archive_write_disk_new();
+    if (!ext) {
+        archive_read_free(a);
+        return false;
+    }
+
     archive_write_disk_set_options(ext, flags);
     archive_write_disk_set_standard_lookup(ext);
 
@@ -32,30 +41,36 @@ bool ArchiveUtils::extract_tar_gz_from_file(const std::string& archive_path, con
         r = archive_read_next_header(a, &entry);
         if (r == ARCHIVE_EOF)
             break;
-        if (r < ARCHIVE_OK)
+        if (r < ARCHIVE_WARN) {
+            archive_read_free(a);
+            archive_write_free(ext);
             return false;
-        if (r < ARCHIVE_WARN)
-            return false;
+        }
 
         const char* current_file = archive_entry_pathname(entry);
         std::string full_path = destination_path + "/" + current_file;
         archive_entry_set_pathname(entry, full_path.c_str());
 
         r = archive_write_header(ext, entry);
-        if (r < ARCHIVE_OK)
+        if (r < ARCHIVE_OK) {
+            archive_read_free(a);
+            archive_write_free(ext);
             return false;
+        }
         else if (archive_entry_size(entry) > 0) {
             r = copy_data(a, ext);
-            if (r < ARCHIVE_OK)
+            if (r < ARCHIVE_WARN) {
+                archive_read_free(a);
+                archive_write_free(ext);
                 return false;
-            if (r < ARCHIVE_WARN)
-                return false;
+            }
         }
         r = archive_write_finish_entry(ext);
-        if (r < ARCHIVE_OK)
+        if (r < ARCHIVE_WARN) {
+            archive_read_free(a);
+            archive_write_free(ext);
             return false;
-        if (r < ARCHIVE_WARN)
-            return false;
+        }
     }
 
     archive_read_close(a);
