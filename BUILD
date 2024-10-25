70dae01e245a4accc65b1ee879358aa415dfb646
Harisaran G <harithiru2018@gmail.com>
2024-10-25 12:35:15 +0530

    add: support for .tar.gz files (#2028)
    * add: support for .tar.gz files
    
    * refactor: binary_body parsing and move content-type to http_server.h
    
    * add: libarchive
    
    * update: BUILD file
    
    * fxi: bazel convecntions
    
    * add: archive utils helper classes
    
    * fix: memory leaks
    
    * refactor: change the flow to cleanup everything at last
    
    * add: archiver tests
    
    * rm: test endpoints
    
    * fix: set data-dir on ArchiveUtilsTest
    
    * add: a const value in http_data.h
    
    * fix: a const value in http_data.h

--- BUILD
+++ BUILD
@@ -64,6 +64,7 @@ cc_library(
         "@whisper.cpp//:whisper_headers",
         "@snowball",
         "@snowball//:snowball_headers",
+        "@archive",
         # "@zip",
     ])
 
--- src/http_server.cpp
+++ src/http_server.cpp
@@ -510,9 +510,16 @@ int HttpServer::catch_all_handler(h2o_handler_t *_h2o_handler, h2o_req_t *req) {
         }
     }
 
+    ssize_t content_type_header_cursor = h2o_find_header_by_str(&req->headers, http_req::CONTENT_TYPE_HEADER, strlen(http_req::CONTENT_TYPE_HEADER), -1);
+    bool is_binary_body = false;
+    if (content_type_header_cursor != -1) {
+        h2o_iovec_t& slot = req->headers.entries[content_type_header_cursor].value;
+        is_binary_body = (std::string(slot.base) == http_req::OCTET_STREAM_HEADER_VALUE);
+    }
+
     std::shared_ptr<http_req> request = std::make_shared<http_req>(req, rpath->http_method, path_without_query,
                                                                    route_hash, query_map, embedded_params_vec,
-                                                                   api_auth_key_sent, body, client_ip);
+                                                                   api_auth_key_sent, body, client_ip, is_binary_body);
 
     // add custom generator with a dispose function for cleaning up resources
     h2o_custom_generator_t* custom_gen = new h2o_custom_generator_t;
