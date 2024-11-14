099e404fea150d6c3015a5f0fb601a514f812fa4
ozanarmagan <o.armagan2020@gtu.edu.tr>
2024-11-14 16:22:59 +0300

    Fix unique lock bug

--- src/image_embedder.cpp
+++ src/image_embedder.cpp
@@ -54,7 +54,6 @@ embedding_res_t CLIPImageEmbedder::embed(const std::string& encoded_image) {
 
 
 std::vector<embedding_res_t> CLIPImageEmbedder::batch_embed(const std::vector<std::string>& inputs) {
-    std::unique_lock<std::mutex> lock(mutex_);
     std::vector<processed_image_t> processed_images;
     std::unordered_map<int, embedding_res_t> results;
 
