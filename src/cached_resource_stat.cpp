b26a856827abb127125386c6702be64c612106b0
Kishore Nallan <kishorenc@gmail.com>
2024-11-23 11:19:07 +0530

    Refactor memory stats calculations.

--- src/cached_resource_stat.cpp
+++ src/cached_resource_stat.cpp
@@ -32,24 +32,11 @@ cached_resource_stat_t::get_resource_status(const std::string& data_dir_path, co
     uint64_t disk_total_bytes = 0;
     uint64_t disk_used_bytes = 0;
 
-    uint64_t memory_total_bytes = 0;
-    uint64_t memory_available_bytes = 0;
-
-    uint64_t swap_total_bytes = 0;
-    uint64_t swap_free_bytes = 0;
-
     // get disk usage
     struct statvfs st{};
     statvfs(data_dir_path.c_str(), &st);
     disk_total_bytes = st.f_blocks * st.f_frsize;
     disk_used_bytes = (st.f_blocks - st.f_bavail) * st.f_frsize;
-
-    SystemMetrics::get_proc_meminfo(memory_total_bytes, memory_available_bytes, swap_total_bytes, swap_free_bytes);
-
-    if(memory_total_bytes == 0) {
-        // if there is an error in fetching the stat, we will return `OK`
-        return cached_resource_stat_t::OK;
-    }
 
     double disk_used_percentage = (double(disk_used_bytes)/double(disk_total_bytes)) * 100;
     if(disk_used_percentage > disk_used_max_percentage) {
@@ -59,21 +46,29 @@ cached_resource_stat_t::get_resource_status(const std::string& data_dir_path, co
         return cached_resource_stat_t::OUT_OF_DISK;
     }
 
-    // Calculate sum of RAM + SWAP used as all_memory_used
-    uint64_t all_memory_used = (memory_total_bytes - memory_available_bytes) + (swap_total_bytes - swap_free_bytes);
+    uint64_t memory_total_bytes = SystemMetrics::get_instance().get_memory_total_bytes();
+    uint64_t memory_used_bytes = SystemMetrics::get_instance().get_memory_used_bytes();
 
-    if(all_memory_used >= memory_total_bytes) {
+    //LOG(INFO) << "memory_total_bytes: " << memory_total_bytes << ", memory_used_bytes: " << memory_used_bytes;
+
+    if(memory_used_bytes == 0) {
+        // if there is an error in fetching the stat, we will return `OK`
+        return cached_resource_stat_t::OK;
+    }
+
+    if(memory_used_bytes >= memory_total_bytes) {
         return cached_resource_stat_t::OUT_OF_MEMORY;
     }
 
     // compare with 500M or `100 - memory_used_max_percentage` of total memory, whichever is lower
     uint64_t memory_free_min_bytes = std::min<uint64_t>(500ULL * 1024 * 1024,
                                                         ((100ULL - memory_used_max_percentage) * memory_total_bytes) / 100);
-    uint64_t free_mem = (memory_total_bytes - all_memory_used);
+    uint64_t free_mem = (memory_total_bytes - memory_used_bytes);
 
     if(free_mem < memory_free_min_bytes) {
+        auto memory_available_bytes = (memory_total_bytes - memory_used_bytes);
         LOG(INFO) << "memory_total: " << memory_total_bytes << ", memory_available: " << memory_available_bytes
-                  << ", all_memory_used: " << all_memory_used << ", free_mem: " << free_mem
+                  << ", all_memory_used: " << memory_used_bytes << ", free_mem: " << free_mem
                   << ", memory_free_min: " << memory_free_min_bytes;
         return cached_resource_stat_t::OUT_OF_MEMORY;
     }
