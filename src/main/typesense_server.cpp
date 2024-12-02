cb353b1e8d22794ccc8e60e7271255b8fc73d1fe
krunal <krunal1313@gmail.com>
2024-12-02 13:20:39 +0530

    make dictionary post request async

--- src/main/typesense_server.cpp
+++ src/main/typesense_server.cpp
@@ -85,7 +85,7 @@ void master_server_routes() {
     server->get("/analytics/events", get_analytics_events);
 
     // for plurals, nouns
-    server->post("/stemming/dictionary/import", post_import_dictionary);
+    server->post("/stemming/dictionary/import", post_import_dictionary, true, true);
 
     // meta
     server->get("/metrics.json", get_metrics_json);
