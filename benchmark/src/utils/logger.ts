9d990cab8d30040f6cf842217d9befe0fcaed13e
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-12-30 17:10:25 +0200

    fix(bench): remove unused eslint ignore comments

--- benchmark/src/utils/logger.ts
+++ benchmark/src/utils/logger.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 import chalk from "chalk";
 
 export const LogLevel = {
