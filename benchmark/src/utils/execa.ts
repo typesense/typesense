33a5e054289f7912a851648e3e5c2c7cdb91dfb7
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-12-30 12:39:48 +0200

    feat(utils): add utility for wrapping execa with result type

---
+++ benchmark/src/utils/execa.ts
@@ -0,0 +1,20 @@
+import type { ErrorWithMessage } from "@/utils/error";
+import type { Result as ExecaResult, Options } from "execa";
+
+import { execa } from "execa";
+import { ResultAsync } from "neverthrow";
+
+import { toErrorWithMessage } from "@/utils/error";
+
+function safeExeca(
+  command: string,
+  args: string[],
+  opts?: Options,
+): ResultAsync<ExecaResult<Options>, ErrorWithMessage> {
+  return ResultAsync.fromPromise(
+    execa(command, args, opts),
+    toErrorWithMessage,
+  );
+}
+
+export { safeExeca };
