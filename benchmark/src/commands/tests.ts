4379a5d6343e20dce027dd683baafb142dad890b
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-02-06 10:49:53 +0200

    fix: uncomment conversation test execution

--- benchmark/src/commands/tests.ts
+++ benchmark/src/commands/tests.ts
@@ -569,8 +569,8 @@ const test = new Command()
       .andThen((integrationTests) =>
         integrationTests
           .setupProcesses()
-          // .andThen(() => integrationTests.conversationTest())
-          // .andThen(() => integrationTests.emptyDataDirectories())
+          .andThen(() => integrationTests.conversationTest())
+          .andThen(() => integrationTests.emptyDataDirectories())
           .andThen(() => integrationTests.snapshotTest())
           .andThen(() => integrationTests.emptyDataDirectories())
           .andThen(() => integrationTests.openAIEmbeddingTest())
