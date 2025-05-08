e456676ba941522c71ad7c9680ae81d2b80aef4c
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-05-08 17:33:05 +0300

    fix(client): adjust client retry parameters for typesense process
    - reduce `retryIntervalSeconds` from 10 to 3 seconds
    - increase `numRetries` from 6 to 20 attempts

--- tests/src/typesense-process.ts
+++ tests/src/typesense-process.ts
@@ -71,8 +71,8 @@ export class TypesenseProcessController extends EventEmitter {
       logLevel: "DEBUG",
       apiKey: this.apiKey,
       connectionTimeoutSeconds: 100,
-      retryIntervalSeconds: 10,
-      numRetries: 6,
+      retryIntervalSeconds: 3,
+      numRetries: 20,
     });
 
     this.process.on("close", (code, signal) => {
