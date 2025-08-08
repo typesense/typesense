8b6930f23ad8e827e4674dbd357abcc0dece39e3
Felipe Hertzer <felipe@hertzer.com.br>
2025-08-08 21:23:19 +1000

    add extra tests

--- test/collection_filtering_test.cpp
+++ test/collection_filtering_test.cpp
@@ -232,6 +232,28 @@ TEST_F(CollectionFilteringTest, FilterByExactPhraseMatch) {
     ASSERT_EQ("1", results["hits"][1]["document"]["id"].get<std::string>());
 }
 
+TEST_F(CollectionFilteringTest, FilterByNegatedExactPhraseMatch) {
+    Collection *coll;
+    std::vector<field> fields = {
+            field("text", field_types::STRING, false)
+    };
+    coll = collectionManager.create_collection("coll_negated_phrase", 1, fields, "").get();
+
+    coll->add(R"({"id": "1", "text": "this is a test"})");
+    coll->add(R"({"id": "2", "text": "this is not a test"})");
+    coll->add(R"({"id": "3", "text": "another test case"})");
+
+    auto results = coll->search("*", {"text"}, "text:!=\"this is a test\"", {}, {}, {0}, 10, 1, FREQUENCY, {false}).get();
+    ASSERT_EQ(2, results["found"].get<size_t>());
+
+    std::set<std::string> expected_ids = {"2", "3"};
+    std::set<std::string> actual_ids;
+    for(const auto& hit : results["hits"]) {
+        actual_ids.insert(hit["document"]["id"].get<std::string>());
+    }
+    ASSERT_EQ(expected_ids, actual_ids);
+}
+
 TEST_F(CollectionFilteringTest, FilterByExactPhraseMatchInArray) {
     Collection *coll;
     std::vector<field> fields = {
@@ -255,21 +277,22 @@ TEST_F(CollectionFilteringTest, FilterByExactPhraseMatchInArray) {
     ASSERT_EQ(expected_ids, actual_ids);
 }
 
-TEST_F(CollectionFilteringTest, FilterByNegatedExactPhraseMatch) {
+TEST_F(CollectionFilteringTest, FilterByNegatedExactPhraseMatchInArray) {
     Collection *coll;
     std::vector<field> fields = {
-            field("text", field_types::STRING, false)
+            field("tags", field_types::STRING_ARRAY, true)
     };
-    coll = collectionManager.create_collection("coll_negated_phrase", 1, fields, "").get();
+    coll = collectionManager.create_collection("coll_phrase_array_negated", 1, fields, "").get();
 
-    coll->add(R"({"id": "1", "text": "this is a test"})");
-    coll->add(R"({"id": "2", "text": "this is not a test"})");
-    coll->add(R"({"id": "3", "text": "another test case"})");
+    coll->add(R"({"id": "1", "tags": ["new york", "travel"]})");
+    coll->add(R"({"id": "2", "tags": ["new", "york", "travel"]})");
+    coll->add(R"({"id": "3", "tags": ["paris", "travel"]})");
+    coll->add(R"({"id": "4", "tags": ["new york", "paris"]})");
 
-    auto results = coll->search("*", {"text"}, "text:!=\"this is a test\"", {}, {}, {0}, 10, 1, FREQUENCY, {false}).get();
-    ASSERT_EQ(2, results["found"].get<size_t>());
+    auto results = coll->search("*", {"tags"}, "tags:!=[\"new york\", paris]", {}, {}, {0}, 10, 1, FREQUENCY, {false}).get();
+    ASSERT_EQ(1, results["found"].get<size_t>());
 
-    std::set<std::string> expected_ids = {"2", "3"};
+    std::set<std::string> expected_ids = {"2"};
     std::set<std::string> actual_ids;
     for(const auto& hit : results["hits"]) {
         actual_ids.insert(hit["document"]["id"].get<std::string>());
@@ -318,6 +341,47 @@ TEST_F(CollectionFilteringTest, LazyEvaluationOfFilterBy) {
     ASSERT_EQ(expected_ids, actual_ids);
 }
 
+TEST_F(CollectionFilteringTest, LazyEvaluationOfFilterByNegated) {
+    Collection *coll;
+    std::vector<field> fields = {
+            field("field", field_types::STRING, false)
+    };
+    coll = collectionManager.create_collection("coll_lazy_negated", 1, fields, "").get();
+
+    coll->add(R"({"id": "1", "field": "foo"})");
+    coll->add(R"({"id": "2", "field": "foo bar baz"})");
+    coll->add(R"({"id": "3", "field": "foo bar"})");
+    coll->add(R"({"id": "4", "field": "bar"})");
+    coll->add(R"({"id": "5", "field": "foo bar baz"})");
+    coll->add(R"({"id": "6", "field": "baz"})");
+    coll->add(R"({"id": "7", "field": "foo baz bar"})");
+    coll->add(R"({"id": "8", "field": "foo bar baz"})");
+
+    std::map<std::string, std::string> req_params = {
+            {"collection", "coll_lazy_negated"},
+            {"q", "foo"},
+            {"query_by", "field"},
+            {"filter_by", "field:!=\"foo bar baz\""},
+            {"enable_lazy_filter", "true"}
+    };
+    nlohmann::json embedded_params;
+    std::string json_res;
+    auto now_ts = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::system_clock::now().time_since_epoch()).count();
+
+    auto search_op = collectionManager.do_search(req_params, embedded_params, json_res, now_ts);
+    ASSERT_TRUE(search_op.ok());
+    auto res_obj = nlohmann::json::parse(json_res);
+    ASSERT_EQ(3, res_obj["found"].get<size_t>());
+
+    std::set<std::string> expected_ids = {"1", "3", "7"};
+    std::set<std::string> actual_ids;
+    for(const auto& hit : res_obj["hits"]) {
+        actual_ids.insert(hit["document"]["id"].get<std::string>());
+    }
+    ASSERT_EQ(expected_ids, actual_ids);
+}
+
 TEST_F(CollectionFilteringTest, LazyEvaluationOfFilterByInArray) {
     Collection *coll;
     std::vector<field> fields = {
@@ -352,6 +416,47 @@ TEST_F(CollectionFilteringTest, LazyEvaluationOfFilterByInArray) {
     ASSERT_EQ(4, res_obj["found"].get<size_t>());
 
     std::set<std::string> expected_ids = {"1", "2", "5", "8"};
+    std::set<std::string> actual_ids;
+    for(const auto& hit : res_obj["hits"]) {
+        actual_ids.insert(hit["document"]["id"].get<std::string>());
+    }
+    ASSERT_EQ(expected_ids, actual_ids);
+}
+
+TEST_F(CollectionFilteringTest, LazyEvaluationOfFilterByInArrayNegated) {
+    Collection *coll;
+    std::vector<field> fields = {
+            field("field", field_types::STRING, false)
+    };
+    coll = collectionManager.create_collection("coll_lazy_array_negated", 1, fields, "").get();
+
+    coll->add(R"({"id": "1", "field": "foo"})");
+    coll->add(R"({"id": "2", "field": "foo bar baz"})");
+    coll->add(R"({"id": "3", "field": "foo bar"})");
+    coll->add(R"({"id": "4", "field": "bar"})");
+    coll->add(R"({"id": "5", "field": "foo bar baz"})");
+    coll->add(R"({"id": "6", "field": "baz"})");
+    coll->add(R"({"id": "7", "field": "foo baz bar"})");
+    coll->add(R"({"id": "8", "field": "foo bar baz"})");
+
+    std::map<std::string, std::string> req_params = {
+            {"collection", "coll_lazy_array_negated"},
+            {"q", "foo"},
+            {"query_by", "field"},
+            {"filter_by", "field:!=[foo, bar, baz, \"foo bar baz\"]"},
+            {"enable_lazy_filter", "true"}
+    };
+    nlohmann::json embedded_params;
+    std::string json_res;
+    auto now_ts = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::system_clock::now().time_since_epoch()).count();
+
+    auto search_op = collectionManager.do_search(req_params, embedded_params, json_res, now_ts);
+    ASSERT_TRUE(search_op.ok());
+    auto res_obj = nlohmann::json::parse(json_res);
+    ASSERT_EQ(2, res_obj["found"].get<size_t>());
+
+    std::set<std::string> expected_ids = {"3", "7"};
     std::set<std::string> actual_ids;
     for(const auto& hit : res_obj["hits"]) {
         actual_ids.insert(hit["document"]["id"].get<std::string>());
