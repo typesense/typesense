35ded9ea870775258dcb7fd5763977293f00824f
Harpreet Sangar <happy_san@protonmail.com>
2025-09-27 14:25:22 +0530

    Include referenced document when re-indexed.

--- test/collection_join_test.cpp
+++ test/collection_join_test.cpp
@@ -5562,6 +5562,34 @@ TEST_F(CollectionJoinTest, CascadeDeleteOption) {
     ASSERT_EQ(1, doc["ref_c_sequence_id"][0]);
     ASSERT_EQ(2, doc["ref_c_sequence_id"][1]);
 
+    std::map<std::string, std::string> req_params = {
+            {"collection", "coll_a"},
+            {"q", "*"},
+            {"filter_by", "id: 1"},
+            {"include_fields", "$coll_b(*), $coll_c(*)"}
+    };
+    nlohmann::json embedded_params;
+    std::string json_res;
+    auto now_ts = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::system_clock::now().time_since_epoch()).count();
+
+    auto search_op = collectionManager.do_search(req_params, embedded_params, json_res, now_ts);
+    ASSERT_TRUE(search_op.ok());
+
+    nlohmann::json res_obj = nlohmann::json::parse(json_res);
+    ASSERT_EQ(1, res_obj["found"].get<size_t>());
+    ASSERT_EQ(1, res_obj["hits"].size());
+    ASSERT_EQ("1", res_obj["hits"][0]["document"]["id"]);
+    ASSERT_EQ("b_0", res_obj["hits"][0]["document"]["ref_b"]);
+    ASSERT_EQ("b_0", res_obj["hits"][0]["document"]["coll_b"]["id"]);
+
+    ASSERT_EQ(2, res_obj["hits"][0]["document"]["ref_c"].size());
+    ASSERT_EQ("c_1", res_obj["hits"][0]["document"]["ref_c"][0]);
+    ASSERT_EQ("c_2", res_obj["hits"][0]["document"]["ref_c"][1]);
+    ASSERT_EQ(2, res_obj["hits"][0]["document"]["coll_c"].size());
+    ASSERT_EQ("c_1", res_obj["hits"][0]["document"]["coll_c"][0]["id"]);
+    ASSERT_EQ("c_2", res_obj["hits"][0]["document"]["coll_c"][1]["id"]);
+
     ASSERT_FALSE(coll_a->get_schema()["ref_b"].cascade_delete);
     // With cascade_delete: false, we shouldn't delete any information of referencing document.
     collectionManager.get_collection_unsafe("coll_b")->remove("b_1");
@@ -5610,21 +5638,17 @@ TEST_F(CollectionJoinTest, CascadeDeleteOption) {
     ASSERT_EQ(Join::reference_helper_sentinel_value, doc["ref_c_sequence_id"][0]);
     ASSERT_EQ(2, doc["ref_c_sequence_id"][1]);
 
-    std::map<std::string, std::string> req_params = {
+    req_params = {
             {"collection", "coll_a"},
             {"q", "*"},
             {"filter_by", "id: 1"},
             {"include_fields", "$coll_b(*), $coll_c(*)"}
     };
-    nlohmann::json embedded_params;
-    std::string json_res;
-    auto now_ts = std::chrono::duration_cast<std::chrono::microseconds>(
-            std::chrono::system_clock::now().time_since_epoch()).count();
 
-    auto search_op = collectionManager.do_search(req_params, embedded_params, json_res, now_ts);
+    search_op = collectionManager.do_search(req_params, embedded_params, json_res, now_ts);
     ASSERT_TRUE(search_op.ok());
 
-    nlohmann::json res_obj = nlohmann::json::parse(json_res);
+    res_obj = nlohmann::json::parse(json_res);
     ASSERT_EQ(1, res_obj["found"].get<size_t>());
     ASSERT_EQ(1, res_obj["hits"].size());
     ASSERT_EQ("1", res_obj["hits"][0]["document"]["id"]);
@@ -5636,6 +5660,46 @@ TEST_F(CollectionJoinTest, CascadeDeleteOption) {
     ASSERT_EQ("c_2", res_obj["hits"][0]["document"]["ref_c"][1]);
     ASSERT_EQ(1, res_obj["hits"][0]["document"]["coll_c"].size()); // c_1 has been deleted
     ASSERT_EQ("c_2", res_obj["hits"][0]["document"]["coll_c"][0]["id"]);
+
+    // Referenced document should be included after re-indexing.
+    auto coll_c = collectionManager.get_collection_unsafe("coll_c").get();
+    auto op = coll_c->add(R"({ "id": "c_1" })");
+    ASSERT_TRUE(op.ok());
+
+    doc = coll_a->get("1").get();
+    ASSERT_EQ(1, doc.count(".ref"));
+    ASSERT_EQ(2, doc[".ref"].size());
+    ASSERT_EQ("ref_c_sequence_id", doc[".ref"].at(0));
+    ASSERT_EQ("ref_b_sequence_id", doc[".ref"].at(1));
+    ASSERT_EQ(Join::reference_helper_sentinel_value, doc["ref_b_sequence_id"]);
+    ASSERT_EQ(2, doc["ref_c_sequence_id"].size());
+    ASSERT_EQ(3, doc["ref_c_sequence_id"][0]); // c_1 has been re-indexed
+    ASSERT_EQ(2, doc["ref_c_sequence_id"][1]);
+
+    req_params = {
+            {"collection", "coll_a"},
+            {"q", "*"},
+            {"filter_by", "id: 1"},
+            {"include_fields", "$coll_b(*), $coll_c(*)"}
+    };
+
+    json_res.clear();
+    search_op = collectionManager.do_search(req_params, embedded_params, json_res, now_ts);
+    ASSERT_TRUE(search_op.ok());
+
+    res_obj = nlohmann::json::parse(json_res);
+    ASSERT_EQ(1, res_obj["found"].get<size_t>());
+    ASSERT_EQ(1, res_obj["hits"].size());
+    ASSERT_EQ("1", res_obj["hits"][0]["document"]["id"]);
+    ASSERT_EQ("b_0", res_obj["hits"][0]["document"]["ref_b"]);
+    ASSERT_EQ(0, res_obj["hits"][0]["document"].count("coll_b")); // b_0 has been deleted
+
+    ASSERT_EQ(2, res_obj["hits"][0]["document"]["ref_c"].size());
+    ASSERT_EQ("c_1", res_obj["hits"][0]["document"]["ref_c"][0]);
+    ASSERT_EQ("c_2", res_obj["hits"][0]["document"]["ref_c"][1]);
+    ASSERT_EQ(2, res_obj["hits"][0]["document"]["coll_c"].size()); // c_1 has been re-indexed
+    ASSERT_EQ("c_2", res_obj["hits"][0]["document"]["coll_c"][0]["id"]);
+    ASSERT_EQ("c_1", res_obj["hits"][0]["document"]["coll_c"][1]["id"]);
 }
 
 TEST_F(CollectionJoinTest, CascadeDeletion) {
