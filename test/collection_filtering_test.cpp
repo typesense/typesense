c18280fb907d1db6e02118a7e1f683d08f717599
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-10-10 18:25:42 +0300

    feat(test): update nested filter test to include case variations

--- test/collection_filtering_test.cpp
+++ test/collection_filtering_test.cpp
@@ -3686,22 +3686,22 @@ TEST_F(CollectionFilteringTest, NestedObjectFieldsFiltering) {
     std::vector<nlohmann::json> documents = {
             R"({
                 "name": "Pasta",
-                "ingredients": [{"name": "cheese", "concentration": 40}, {"name" : "spinach", "concentration": 10},
+                "ingredients": [{"name": "Cheese", "concentration": 40}, {"name" : "spinach", "concentration": 10},
                                 {"name": "jalepeno", "concentration": 20}]
             })"_json,
             R"({
                 "name": "Pizza",
-                "ingredients": [{"name": "cheese", "concentration": 30}, {"name": "pizza sauce", "concentration": 30},
+                "ingredients": [{"name": "CHEESE", "concentration": 30}, {"name": "pizza sauce", "concentration": 30},
                                 {"name": "olives", "concentration": 30}]
             })"_json,
             R"({
                 "name": "Lasagna",
-                "ingredients": [{"name": "cheese", "concentration": 60}, {"name": "jalepeno", "concentration": 20},
+                "ingredients": [{"name": "Cheese", "concentration": 60}, {"name": "jalepeno", "concentration": 20},
                                 {"name": "olives", "concentration": 20}]
             })"_json,
             R"({
                 "name": "Popcorn",
-                "ingredients": [{"name": "cheese", "concentration": 30}]
+                "ingredients": [{"name": "CHEESE", "concentration": 30}]
             })"_json,
             R"({
                 "name": "Pizza Rolls",
