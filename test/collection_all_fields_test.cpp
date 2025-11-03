f1f3bdc23dae857f5eaf404f0a196b28061bafdd
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-11-03 12:50:02 +0200

    test: add test for nested auto fields

--- test/collection_all_fields_test.cpp
+++ test/collection_all_fields_test.cpp
@@ -2000,8 +2000,11 @@ TEST_F(CollectionAllFieldsTest, AvoidDuplicateDynamicFields) {
         "fields": [
             {"name": "product", "type": "auto"},
             {"name": "title", "type": "auto"},
-            {"name": "description", "type": "string"}
-        ]
+            {"name": "description", "type": "string"},
+            {"name": "nested.price", "type": "auto", "facet": true},
+            {"name": ".*", "type": "auto"}
+        ],
+        "enable_nested_fields": true
     })"_json;
 
     auto create_op = collectionManager.create_collection(schema);
@@ -2009,16 +2012,44 @@ TEST_F(CollectionAllFieldsTest, AvoidDuplicateDynamicFields) {
 
     Collection* collection = create_op.get();
     auto fields = collection->get_fields();
-    ASSERT_EQ(3, fields.size());
+    ASSERT_EQ(5, fields.size());
 
     nlohmann::json doc;
     doc["id"] = "0";
     doc["product"] = "Running Shoes";
     doc["title"] = "Nike Shoes";
     doc["description"] = "A nice pair of running shoes.";
+    doc["nested"] = R"({"price": 100})"_json;
     
     auto add_op = collection->add(doc.dump(), CREATE);
     ASSERT_TRUE(add_op.ok());
     fields = collection->get_fields();
-    ASSERT_EQ(3, fields.size());
+    // it's going to add the parent `nested` field as well
+    ASSERT_EQ(6, fields.size());
+    ASSERT_EQ(field_types::INT64, fields[5].type);
+    ASSERT_TRUE(fields[5].facet);
+
+    auto search_results = collection->search("*", {}, "nested.price: 100", {}, {}, {0}, 10, 1, FREQUENCY, {false});
+    ASSERT_TRUE(search_results.ok());
+    auto results = search_results.get();
+    ASSERT_EQ(1, results["found"].get<size_t>());
+    ASSERT_EQ(1, results["hits"].size());
+    ASSERT_STREQ("0", results["hits"][0]["document"]["id"].get<std::string>().c_str());
+    
+    auto& document = results["hits"][0]["document"];
+    ASSERT_TRUE(document.contains("nested"));
+    ASSERT_TRUE(document["nested"].is_object());
+    ASSERT_TRUE(document["nested"].contains("price"));
+    ASSERT_EQ(100, document["nested"]["price"].get<int64_t>());
+
+    search_results = collection->search("*", {}, "", {"nested.price"}, {}, {0}, 10, 1, FREQUENCY, {false});
+    ASSERT_TRUE(search_results.ok());
+    results = search_results.get();
+    ASSERT_EQ(1, results["facet_counts"].size());
+    ASSERT_STREQ("nested.price", results["facet_counts"][0]["field_name"].get<std::string>().c_str());
+    ASSERT_EQ(1, results["facet_counts"][0]["counts"].size());
+    ASSERT_STREQ("100", results["facet_counts"][0]["counts"][0]["value"].get<std::string>().c_str());
+    ASSERT_EQ(1, (int)results["facet_counts"][0]["counts"][0]["count"]);
+
+    collectionManager.drop_collection("test");
 }
