9acd0a2c21cd77dfdfbb78c907898284339593e6
krunal1313 <krunal1313@gmail.com>
2026-04-25 20:40:18 +0530

    fix reloading nl models from snapshot recover

--- src/raft_server.cpp
+++ src/raft_server.cpp
@@ -1,5 +1,6 @@
 #include "raft_server.h"
 #include "core_api.h"
+#include "natural_language_search_model_manager.h"
 #include "personalization_model_manager.h"
 #include "rocksdb/utilities/checkpoint.h"
 #include "store.h"
@@ -673,6 +674,15 @@ int ReplicationState::init_db() {
     } else {
         LOG(ERROR)<< "Typesense failed to start. " << "Could not load collections from disk: " << init_op.error();
         return 1;
+    }
+
+    auto natural_language_search_init = NaturalLanguageSearchModelManager::init(store);
+    if(!natural_language_search_init.ok()) {
+        LOG(INFO) << "Failed to initialize natural language search model manager: "
+                  << natural_language_search_init.error();
+    } else {
+        LOG(INFO) << "Loaded " << natural_language_search_init.get()
+                  << " natural language search model(s).";
     }
 
     // important to init conversation models only after all collections have been loaded
